d78f7c0d486e040efc3df539788d0ec60afe63b2
bassemkamalm <basimkamal.android@gmail.com>
2022-07-09 16:19:22 +0200

    commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:4200/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://localhost:4200/api/v0',
 };
 
 /*
