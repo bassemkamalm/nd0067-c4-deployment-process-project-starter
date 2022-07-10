2d306069bac1ad0347e465cdbb68a2a980a77487
bassemkamalm <basimkamal.android@gmail.com>
2022-07-10 14:00:16 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram.c7vlefujubju.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=udagram
 export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
 export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
-export AWS_ACCESS_KEY_ID=AKIAQN5MF5FGM3Z3BLFH
-export AWS_SECRET_ACCESS_KEY=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
