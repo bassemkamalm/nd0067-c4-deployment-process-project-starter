081ed6b39a90eed65374af1e238a9a826f0b1b98
bassemkamalm <basimkamal.android@gmail.com>
2022-07-11 13:00:03 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::udagrambucket01
 export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagram-api-dev22.us-west-2.elasticbeanstalk.com/
